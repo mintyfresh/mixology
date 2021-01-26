875068ebc04ea407cd54231bc523dd4f43a23d79
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2021-01-25 20:41:38 -0500

    Fix error messages for change password flow.

--- api/app/forms/change_password_form.rb
+++ api/app/forms/change_password_form.rb
@@ -9,14 +9,21 @@ class ChangePasswordForm < ApplicationForm
   validates :user, :old_password, presence: true
   validates :new_password, confirmation: true, password: true
   validates :new_password_confirmation, presence: true
+  validate  :old_password_matches_user, if: -> { user && old_password.present? }
 
   # @return [User]
   def perform
-    user = self.user.authenticate(UserPasswordCredential, old_password)
-    return errors.add(:base, :incorrect_credentials) if user.nil?
-
     user.credential(UserPasswordCredential).update!(password: new_password)
 
     user
+  end
+
+private
+
+  # @return [void]
+  def old_password_matches_user
+    return if user.authenticate(UserPasswordCredential, old_password)
+
+    errors.add(:old_password, :incorrect)
   end
 end
--- api/spec/forms/change_password_form_spec.rb
+++ api/spec/forms/change_password_form_spec.rb
@@ -23,6 +23,11 @@ RSpec.describe ChangePasswordForm, type: :form do
     expect(form).to be_invalid
   end
 
+  it 'is invalid when the old password is incorrect' do
+    input[:old_password] = 'incorrect-password'
+    expect(form).to be_invalid
+  end
+
   it 'is invalid without a new password' do
     input[:new_password] = nil
     expect(form).to be_invalid
@@ -64,34 +69,6 @@ RSpec.describe ChangePasswordForm, type: :form do
     it 'marks the password credential as having been changed' do
       freeze_time do
         expect { perform }.to change { user.credential(UserPasswordCredential).last_changed_at }.to(Time.current)
-      end
-    end
-
-    context 'when the old password is incorrect' do
-      let(:user) { create(:user, :with_password) }
-
-      it 'returns a list of errors' do
-        expect(perform).to be_a(ActiveModel::Errors)
-          .and be_of_kind(:base, :incorrect_credentials)
-      end
-
-      it "does not change the user's password to the new password" do
-        perform
-        expect(user.authenticate(UserPasswordCredential, input[:new_password])).to be_falsey
-      end
-    end
-
-    context 'when the user does not have an existing password' do
-      let(:user) { create(:user) }
-
-      it 'returns a list of errors' do
-        expect(perform).to be_a(ActiveModel::Errors)
-          .and be_of_kind(:base, :incorrect_credentials)
-      end
-
-      it "does not change the user's password to the new password" do
-        perform
-        expect(user.authenticate(UserPasswordCredential, input[:new_password])).to be_falsey
       end
     end
   end
