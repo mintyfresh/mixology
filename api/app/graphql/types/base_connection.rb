4c912b470854fb54f0cdf7d3d12e2ab52e20f8c1
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2021-01-27 22:19:26 -0500

    Update dependencies.

--- api/app/graphql/types/base_connection.rb
+++ api/app/graphql/types/base_connection.rb
@@ -11,11 +11,12 @@ module Types
       # Set this connection's graphql name
       node_type_name = node_type.graphql_name
 
-      @node_type = node_type
-      @edge_type = edge_type_class
+      @node_type  = node_type
+      @edge_type  = edge_type_class
+      @edge_class = edge_class
 
       field :edges, [edge_type_class], null: false, description: 'A list of edges.',
-                                       method: :edge_nodes, edge_class: edge_class
+                                       legacy_edge_class: edge_class, connection: false
 
       field :nodes, [node_type], null: false, description: 'A list of nodes.' if nodes_field
 
