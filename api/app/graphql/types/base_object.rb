57c657edb926c306cfbdfd0dfaeda303e4690098
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2021-01-30 22:38:42 -0500

    Include missing authorization module.

--- api/app/graphql/types/base_object.rb
+++ api/app/graphql/types/base_object.rb
@@ -6,6 +6,8 @@ module Types
       include ActionView::Helpers::NumberHelper
     end
 
+    include Authorization
+
     field_class Types::BaseField
     connection_type_class Types::BaseConnection
     edge_type_class Types::BaseEdge
