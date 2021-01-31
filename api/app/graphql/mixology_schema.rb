dc662a0d3b0da82d37e6100912804ab59e9d4c5d
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2021-01-30 19:38:39 -0500

    Remove default installed middleware.

--- api/app/graphql/mixology_schema.rb
+++ api/app/graphql/mixology_schema.rb
@@ -4,18 +4,8 @@ class MixologySchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
-  # Opt in to the new runtime (default in future graphql-ruby versions)
-  use GraphQL::Execution::Interpreter
-  use GraphQL::Analysis::AST
-
-  # Add `rescue_from` hooks to GraphQL
-  use GraphQL::Execution::Errors
-
   # Load records from the database in batches
   use GraphQL::Batch
-
-  # Add built-in connections for pagination
-  use GraphQL::Pagination::Connections
 
   rescue_from(ActiveRecord::RecordNotFound) do |_, _, _, _, field|
     raise GraphQL::ExecutionError.new("#{field.type.unwrap.graphql_name} not found", extensions: { code: 'NOT_FOUND' })
